e90e104963f886934d0933403a03aa0defb45aa1
DrVector-000 <93315905+DrVector-000@users.noreply.github.com>
2022-03-29 12:16:32 +0200

    Initial commit

---
+++ Windows/SerialEepromManager/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace SerialEepromManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cBSerialPorts = new System.Windows.Forms.ComboBox();
+            this.bConnetti = new System.Windows.Forms.Button();
+            this.tBInfo = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.bDisconnetti = new System.Windows.Forms.Button();
+            this.bReadAll = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.bWriteAll = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cBSerialPorts
+            // 
+            this.cBSerialPorts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cBSerialPorts.FormattingEnabled = true;
+            this.cBSerialPorts.Location = new System.Drawing.Point(398, 23);
+            this.cBSerialPorts.Name = "cBSerialPorts";
+            this.cBSerialPorts.Size = new System.Drawing.Size(108, 21);
+            this.cBSerialPorts.TabIndex = 0;
+            this.cBSerialPorts.SelectedIndexChanged += new System.EventHandler(this.cBSerialPorts_SelectedIndexChanged);
+            // 
+            // bConnetti
+            // 
+            this.bConnetti.Enabled = false;
+            this.bConnetti.Location = new System.Drawing.Point(398, 50);
+            this.bConnetti.Name = "bConnetti";
+            this.bConnetti.Size = new System.Drawing.Size(108, 23);
+            this.bConnetti.TabIndex = 1;
+            this.bConnetti.Text = "Connetti";
+            this.bConnetti.UseVisualStyleBackColor = true;
+            this.bConnetti.Click += new System.EventHandler(this.bConnetti_Click);
+            // 
+            // tBInfo
+            // 
+            this.tBInfo.BackColor = System.Drawing.SystemColors.Info;
+            this.tBInfo.Location = new System.Drawing.Point(12, 12);
+            this.tBInfo.Multiline = true;
+            this.tBInfo.Name = "tBInfo";
+            this.tBInfo.ReadOnly = true;
+            this.tBInfo.Size = new System.Drawing.Size(367, 228);
+            this.tBInfo.TabIndex = 2;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 255);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(533, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(142, 17);
+            this.toolStripStatusLabel1.Text = "Dispositivo non connesso";
+            // 
+            // bDisconnetti
+            // 
+            this.bDisconnetti.Enabled = false;
+            this.bDisconnetti.Location = new System.Drawing.Point(398, 79);
+            this.bDisconnetti.Name = "bDisconnetti";
+            this.bDisconnetti.Size = new System.Drawing.Size(108, 23);
+            this.bDisconnetti.TabIndex = 1;
+            this.bDisconnetti.Text = "Disconnetti";
+            this.bDisconnetti.UseVisualStyleBackColor = true;
+            this.bDisconnetti.Click += new System.EventHandler(this.bDisconnetti_Click);
+            // 
+            // bReadAll
+            // 
+            this.bReadAll.Enabled = false;
+            this.bReadAll.Location = new System.Drawing.Point(398, 144);
+            this.bReadAll.Name = "bReadAll";
+            this.bReadAll.Size = new System.Drawing.Size(108, 23);
+            this.bReadAll.TabIndex = 1;
+            this.bReadAll.Text = "Leggi intera ROM";
+            this.bReadAll.UseVisualStyleBackColor = true;
+            this.bReadAll.Click += new System.EventHandler(this.bReadAll_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "File bin|*.bin|Tutti i file|*.*";
+            this.saveFileDialog1.Title = "Salva ROM";
+            // 
+            // bWriteAll
+            // 
+            this.bWriteAll.Enabled = false;
+            this.bWriteAll.Location = new System.Drawing.Point(398, 173);
+            this.bWriteAll.Name = "bWriteAll";
+            this.bWriteAll.Size = new System.Drawing.Size(108, 23);
+            this.bWriteAll.TabIndex = 1;
+            this.bWriteAll.Text = "Scrivi intera ROM";
+            this.bWriteAll.UseVisualStyleBackColor = true;
+            this.bWriteAll.Click += new System.EventHandler(this.bWriteAll_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "File bin|*.bin|Tutti i file|*.*";
+            this.openFileDialog1.Title = "Scrivi ROM";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 277);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tBInfo);
+            this.Controls.Add(this.bWriteAll);
+            this.Controls.Add(this.bReadAll);
+            this.Controls.Add(this.bDisconnetti);
+            this.Controls.Add(this.bConnetti);
+            this.Controls.Add(this.cBSerialPorts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cBSerialPorts;
+        private System.Windows.Forms.Button bConnetti;
+        private System.Windows.Forms.TextBox tBInfo;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Button bDisconnetti;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button bReadAll;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button bWriteAll;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Windows/SerialEepromManager/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SerialEepromManager
+{
+    public partial class Form1 : Form
+    {
+
+        #region Variabili (accesso privato)
+
+        private SerialPort _serialPort;
+
+        #endregion
+
+        #region Costruttore
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Inizializza porta seriale
+            _serialPort = new SerialPort();
+            _serialPort.BaudRate = 115200;
+            _serialPort.DataBits = 8;
+            _serialPort.Parity = Parity.None;
+            _serialPort.StopBits = StopBits.One;
+            _serialPort.PortName = "COM10";
+            _serialPort.DtrEnable = false;
+
+            // Enumerazione delle porte seriali disponibili
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                cBSerialPorts.Items.Add(port);
+            }
+            if (cBSerialPorts.Items.Count > 0)
+            {
+                cBSerialPorts.SelectedIndex = 0;
+                bConnetti.Enabled = true;
+            }
+        }
+
+        #endregion
+
+        #region Handler Eventi Form
+
+        private void cBSerialPorts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            _serialPort.PortName = cBSerialPorts.SelectedItem.ToString();
+            bConnetti.Enabled = true;
+        }
+
+        private void bConnetti_Click(object sender, EventArgs e)
+        {
+            if (Connetti())
+            {
+                string version = LeggiVersione();
+                if (version != "")
+                {
+                    tBInfo.Text = "";
+                    tBInfo.AppendText("Firmware version " + version + "\n");
+                    tBInfo.AppendText("-------------------------------------------------\n");
+                    tBInfo.AppendText("\n");
+
+                    bConnetti.Enabled = false;
+                    bDisconnetti.Enabled = true;
+                    bReadAll.Enabled = true;
+                    bWriteAll.Enabled = true;
+
+                    toolStripStatusLabel1.Text = "Dispositivo connesso";
+                }
+                else
+                {
+                    Disconnetti();
+                }
+            }
+        }
+
+        private void bDisconnetti_Click(object sender, EventArgs e)
+        {
+            if (Disconnetti())
+            {
+                bWriteAll.Enabled = false;
+                bReadAll.Enabled = false;
+                bDisconnetti.Enabled = false;
+                bConnetti.Enabled = true;
+
+                toolStripStatusLabel1.Text = "Dispositivo non connesso";
+            }
+        }
+
+        private void bReadAll_Click(object sender, EventArgs e)
+        {
+            List<byte> datas = new List<byte>();
+            bool b = ReadAll(datas);
+
+            string s = "";
+            int index = 0;
+            for (int i = 0; i < datas.Count; i++)
+            {
+                byte data = datas[i];
+                if (index < 15)
+                {
+                    s += data.ToString("X2") + " ";
+                    index++;
+                }
+                else
+                {
+                    s += data.ToString("X2") + "\r\n";
+                    tBInfo.AppendText(s);
+                    s = "";
+                    index = 0;
+                }
+            }
+
+            saveFileDialog1.FileName = "ROM.bin";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (BinaryWriter writer = new BinaryWriter(File.Open(saveFileDialog1.FileName, FileMode.Create)))
+                {
+                    byte[] buffer = datas.ToArray();
+                    writer.Write(buffer, 0, buffer.Length);
+                }
+            }
+        }
+
+        private void bWriteAll_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openFileDialog1.FileName = "ROM.bin";
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    FileInfo fi = new FileInfo(openFileDialog1.FileName);
+
+                    _serialPort.DiscardInBuffer();
+                    _serialPort.DiscardOutBuffer();
+
+                    using (BinaryReader reader = new BinaryReader(File.Open(openFileDialog1.FileName, FileMode.Open)))
+                    {
+                        byte[] buffer = new byte[fi.Length];
+                        reader.Read(buffer, 0, (int)fi.Length);
+                        WriteAll(buffer);
+                    }
+                }
+            }
+            catch { }
+        }
+
+        #endregion
+
+        #region Metodi (accesso privato)
+
+        private bool Connetti()
+        {
+            try
+            {
+                _serialPort.Open();
+                Thread.Sleep(1000);
+                return _serialPort.IsOpen;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private bool Disconnetti()
+        {
+            _serialPort.Close();
+            return !_serialPort.IsOpen;
+        }
+
+        private string LeggiVersione()
+        {
+            string s = "";
+            try
+            {
+                _serialPort.Write("VERSION=?\r");
+
+                // 5 secondi di timeout
+                int expiredTick = Environment.TickCount + 5000;
+                while (Environment.TickCount < expiredTick)
+                {
+                    if (_serialPort.BytesToRead > 0)
+                    {
+                        s = _serialPort.ReadLine();
+                        string[] split = s.Trim('\r').Split(new char[] { '=' });
+                        if (split[0] == "+VERSION")
+                        {
+                            s = split[1];
+                            break;
+                        }
+                    }
+                }
+
+                return s;
+            }
+            catch
+            {
+                return s;
+            }
+        }
+
+        private bool ReadAll(List<byte> datas)
+        {
+            try
+            {
+                _serialPort.Write("READALL=?\r");
+
+                // 64 Kb = 8192 Byte
+                byte[] buffer = new byte[0x2000];
+                int offset = 0;
+
+                while (true)
+                {
+                    int count = _serialPort.BytesToRead;
+                    if (count > 0)
+                    {
+                        _serialPort.Read(buffer, offset, count);
+                        offset += count;
+                    }
+
+                    if (offset == 0x2000)
+                    {
+                        datas.AddRange(buffer);
+                        break;
+                    }
+                }
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private bool WriteAll(byte[] buffer)
+        {
+            try
+            {
+                // 32 byte per pagina massimo che sembrano comprensivi dei 2 byte dell'indirizzo
+                // quindi scriviamo blocchi di 16 byte per sicurezza
+                int npag = buffer.Length / 16;
+
+                for (int i = 0; i < npag; i++)
+                {
+                    int addr = i * 16;
+                    _serialPort.Write("WRITEBUFFER=" + addr.ToString() + ",16\r");
+                    //Thread.Sleep(1);
+
+                    for (int j = addr; j < addr + 16; j++)
+                    {
+                        //Debug.Print("Address: " + j.ToString() + " - Byte: " + buffer[j].ToString("X2"));
+                        _serialPort.Write(buffer, j, 1);
+                        Thread.Sleep(1);
+                    }
+
+                    //Thread.Sleep(1);
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        #endregion
+
+    }
+}
